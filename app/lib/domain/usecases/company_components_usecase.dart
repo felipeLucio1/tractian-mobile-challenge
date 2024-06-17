f268841ad3f989f0de97df98e2636eafa5f58f18
felipe lucio <felipelucio2009@gmail.com>
2024-06-17 18:46:27 -0300

    feat: cria novo usecase para recuperação de componentes sem pai

---
+++ app/lib/domain/usecases/company_components_usecase.dart
@@ -0,0 +1,36 @@
+import 'package:app/data/model/company_components.dart';
+import 'package:app/data/model/company.dart';
+import 'package:app/domain/usecases/asset_usecase.dart';
+import 'package:app/domain/usecases/company_usecase.dart';
+import 'package:app/domain/usecases/component_usecase.dart';
+import 'package:app/domain/usecases/location_usecase.dart';
+import 'package:injectable/injectable.dart';
+
+@injectable
+class CompanyComponentsUsecase {
+  CompanyComponentsUsecase({
+    required AssetUsecase assetUseCase,
+    required CompanyUsecase companyUsecase,
+    required ComponentUsecase componentUseCase,
+    required LocationUsecase locationUseCase,
+  })  : _componentUsecase = componentUseCase,
+        _locationUsecase = locationUseCase;
+
+  late final ComponentUsecase _componentUsecase;
+  late final LocationUsecase _locationUsecase;
+
+  Stream<CompanyComponents> getAllCompanyAloneComponents(
+      Company company) async* {
+    final CompanyComponents companyComponents;
+    final aloneComponents = _componentUsecase.getAloneComponents(company.id);
+    final locationsGoupList =
+        _locationUsecase.getCompanyAloneLocations(company.id);
+
+    companyComponents = CompanyComponents(
+        company: company,
+        aloneAssets: aloneComponents,
+        locationsGoupList: locationsGoupList);
+
+    yield companyComponents;
+  }
+}
